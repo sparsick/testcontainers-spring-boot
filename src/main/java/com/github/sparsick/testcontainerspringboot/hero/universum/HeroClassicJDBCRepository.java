28f7ecf26594a28d1f19d1bb91f67cc2745902b6
sparsick <sparsick@web.de>
2020-05-01 17:48:13 +0200

    formatting

--- src/main/java/com/github/sparsick/testcontainerspringboot/hero/universum/HeroClassicJDBCRepository.java
+++ src/main/java/com/github/sparsick/testcontainerspringboot/hero/universum/HeroClassicJDBCRepository.java
@@ -1,26 +1,31 @@
 package com.github.sparsick.testcontainerspringboot.hero.universum;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.Collection;
 
+@Repository
 public class HeroClassicJDBCRepository {
 
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     public HeroClassicJDBCRepository(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public void addHero(Hero hero) {
-        jdbcTemplate.update("insert into hero (city, name, universum) values (?,?,?)", hero.getCity(), hero.getName(), hero.getUniversum().name());
+        jdbcTemplate.update("insert into hero (city, name, universum) values (?,?,?)",
+                hero.getCity(), hero.getName(), hero.getUniversum().name());
 
     }
 
     public Collection<Hero> allHeros() {
         return jdbcTemplate.query("select * From hero",
-                (resultSet, i) -> new Hero(resultSet.getString("name"), resultSet.getString("city"), ComicUniversum.valueOf(resultSet.getString("universum"))));
+                (resultSet, i) -> new Hero(resultSet.getString("name"),
+                                            resultSet.getString("city"),
+                                            ComicUniversum.valueOf(resultSet.getString("universum"))));
     }
 
 }
--- src/test/java/com/github/sparsick/testcontainerspringboot/hero/universum/HeroClassicJDBCRepositoryTest.java
+++ src/test/java/com/github/sparsick/testcontainerspringboot/hero/universum/HeroClassicJDBCRepositoryTest.java
@@ -1,12 +1,9 @@
 package com.github.sparsick.testcontainerspringboot.hero.universum;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import com.zaxxer.hikari.HikariDataSource;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.delegate.DatabaseDelegate;
 import org.testcontainers.ext.ScriptUtils;
 import org.testcontainers.jdbc.JdbcDatabaseDelegate;
 import org.testcontainers.junit.jupiter.Container;
@@ -16,26 +13,19 @@ import javax.sql.DataSource;
 import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
 class HeroClassicJDBCRepositoryTest {
-
-
     @Container
     private MySQLContainer database = new MySQLContainer();
 
-
     private HeroClassicJDBCRepository repositoryUnderTest;
-
-    @BeforeEach
-    void setUp(){
-        repositoryUnderTest = new HeroClassicJDBCRepository(dataSource());
-        ScriptUtils.runInitScript(new JdbcDatabaseDelegate(database, ""),"ddl.sql");
-    }
 
     @Test
     void testInteractionWithDatabase() {
+        ScriptUtils.runInitScript(new JdbcDatabaseDelegate(database, ""),"ddl.sql");
+        repositoryUnderTest = new HeroClassicJDBCRepository(dataSource());
+
         repositoryUnderTest.addHero(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
 
         Collection<Hero> heroes = repositoryUnderTest.allHeros();
