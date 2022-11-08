20a02827fcd3761de0af71a23ad32d8a575f439a
Sandra Parsick <mail@sandra-parsick.de>
2022-11-08 14:33:46 +0100

    refactor: remove unused code

--- src/main/java/com/github/sparsick/testcontainerspringboot/hero/universum/NewHeroModel.java
+++ src/main/java/com/github/sparsick/testcontainerspringboot/hero/universum/NewHeroModel.java
@@ -6,8 +6,6 @@ public class NewHeroModel {
 
     private Hero hero;
 
-    private String repository;
-
     public Hero getHero() {
         return hero;
     }
@@ -16,33 +14,24 @@ public class NewHeroModel {
         this.hero = hero;
     }
 
-    public String getRepository() {
-        return repository;
-    }
-
-    public void setRepository(String repository) {
-        this.repository = repository;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         NewHeroModel that = (NewHeroModel) o;
-        return Objects.equals(hero, that.hero) &&
-                Objects.equals(repository, that.repository);
+        return Objects.equals(hero, that.hero);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(hero, repository);
+        return Objects.hash(hero);
     }
 
     @Override
     public String toString() {
         return "NewHeroModel{" +
                 "newHero=" + hero +
-                ", repository='" + repository + '\'' +
                 '}';
     }
 }
